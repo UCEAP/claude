f31e1a57113dd9cbe061ee41af95c14a7a3c13ab
Brandt Kurowski <bkurowski@uceap.universityofcalifornia.edu>
2025-12-02 16:38:22 +0000

    add devcontainer

---
+++ .devcontainer/onCreate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eo pipefail
+
+# Put neovim in the PATH for those who celebrate
+mkdir -p ~/.local/bin
+ln -s /opt/nvim-linux-x86_64/bin/nvim ~/.local/bin
+
+# Install Claude Code
+# TODO inherit this properly, it's already in devcontainer-on-create
+curl -fsSL https://claude.ai/install.sh | bash
+echo -e "export CLAUDE_CODE_USE_FOUNDRY=1\nexport ANTHROPIC_FOUNDRY_RESOURCE=uceap-claude-test-resource" | tee -a ~/.bashrc ~/.zshrc ~/.zshrc.local
+claude plugin marketplace add UCEAP/claude
+claude plugin install uceap
